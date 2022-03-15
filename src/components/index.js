8165b0084b4a855d2f547ab4e99d2cca87abcd30
Eli T. Drumm <eli@eli.td>
2022-03-15 12:55:38 -0400

    init commit, innit

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Header from './Header.svelte'
+import Statement from './Statement.svelte'
+import Skills from './Skills.svelte'
+import Education from './Education.svelte'
+import Employment from './Employment.svelte'
+import References from './References.svelte'
+
+export default {
+  Header,
+  Statement,
+  Skills,
+  Education,
+  Employment,
+  References
+}
---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+import '../app.css'
+
+// import Header from '../components/Header.svelte'
+// import Statement from '../components'
+
+import C from '../components/index'
+</script>
+
+
+<C.Header />
+
+<C.Statement />
+
+
+
